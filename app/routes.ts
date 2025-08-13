04b926da29e951e6e696be75a681e0eee8118f71
sheet848 <sheetalnaik848@gmail.com>
2025-08-13 15:55:00 +0530

    wipe data from cloud page - hidden

--- app/routes.ts
+++ app/routes.ts
@@ -5,4 +5,5 @@ export default [
     route("/auth", "routes/auth.tsx"),
     route('/upload', 'routes/upload.tsx'),
     route('/resume-details/:id', 'routes/resume.tsx'),
+    route('/wipe', 'routes/Wipe.tsx'),
 ] satisfies RouteConfig;
