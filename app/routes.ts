6fdeabf56e2cd50b04e1e6bdd0844ebe681cc6ec
sheet848 <sheetalnaik848@gmail.com>
2025-09-18 17:14:53 +0530

    Auth routes update

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("/auth", "routes/Auth.tsx"),
+    route("/auth", 'routes/Auth.tsx'),
     route('/upload', 'routes/Upload.tsx'),
     route('/resume-details/:id', 'routes/Resume.tsx'),
     route('/wipe', 'routes/Wipe.tsx'),
